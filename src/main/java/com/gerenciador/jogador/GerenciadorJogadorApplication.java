a153e3bb34b7b5caf4873cc12aa8a6b5f0c84770
BPCruzz <bryanparreirascruz2005@gmail.com>
2025-11-04 07:59:34 -0300

    Initial commit

---
+++ src/main/java/com/gerenciador/jogador/GerenciadorJogadorApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.jogador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorJogadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorJogadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciador/jogador/GerenciadorJogadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.jogador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorJogadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
