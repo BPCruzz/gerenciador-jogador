584c625f680e43f7504722aa4159303d819fb93d
BPCruzz <bryanparreirascruz2005@gmail.com>
2025-11-04 08:24:03 -0300

    feat: Adiciona JogadorController com endpoint /teste inicial

---
+++ src/main/java/com/gerenciador/jogador/JogadorController.java
@@ -0,0 +1,23 @@
+package com.gerenciador.jogador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// (1) Diz ao Spring que esta classe vai controlar rotas web (APIs)
+@RestController
+// (2) Define o "prefixo" da URL para esta classe.
+// Todas as rotas aqui dentro vão começar com /jogadores
+@RequestMapping("/jogadores")
+
+
+public class JogadorController {
+
+    // Mapeia este método para requisições GET
+    @GetMapping("/teste")
+    public String fazerTeste() {
+        return "Teste Inicial";
+    }
+
+
+}
