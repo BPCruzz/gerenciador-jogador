7111986a473da1e63757db954c2397ea5f30cb43
BPCruzz <bryanparreirascruz2005@gmail.com>
2025-11-04 08:46:46 -0300

    refactor: Organiza estrutura do projeto
    Cria o controller, model, repository e o service. Move JogadorController
    para o controller.

--- src/main/java/com/gerenciador/jogador/controller/JogadorController.java
+++ src/main/java/com/gerenciador/jogador/controller/JogadorController.java
@@ -1,4 +1,4 @@
-package com.gerenciador.jogador;
+package com.gerenciador.jogador.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
